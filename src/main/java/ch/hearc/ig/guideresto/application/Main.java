c44abe0f6f4581bd9f653cf14dc6eae062cbb1bf
Briefgarde <nemo.vollert@he-arc.ch>
2023-10-24 15:58:50 +0200

    this is a test commit as I pass on VS Code

--- src/main/java/ch/hearc/ig/guideresto/application/Main.java
+++ src/main/java/ch/hearc/ig/guideresto/application/Main.java
@@ -3,7 +3,7 @@ package ch.hearc.ig.guideresto.application;
 import ch.hearc.ig.guideresto.business.City;
 import ch.hearc.ig.guideresto.persistence.FakeItems;
 import ch.hearc.ig.guideresto.persistence.mapper.CityMapper;
-import ch.hearc.ig.guideresto.persistence.mapper.IMapper;
+import ch.hearc.ig.guideresto.persistence.mapper.IMapper; 
 import ch.hearc.ig.guideresto.presentation.CLI;
 
 import java.util.ArrayList;
@@ -18,14 +18,15 @@ public class Main {
     System.out.println("TEst " + test1.getCityName() + test1.getZipCode());
     Set<City> test2 = test.findAll();
     for (City city: test2) {
-      System.out.println("Via find all" + city.getZipCode() + " " + city.getCityName());
+      System.out.println("Via find all" + city.getZipCode() + " " + city.getCityName() + "this is still for a test commit");
     }
-//    var scanner = new Scanner(System.in);
-//    var fakeItems = new FakeItems();
-//    var printStream = System.out;
-//    IMapper cityMapper = new CityMapper();
-//    var cli = new CLI(scanner, printStream, fakeItems, cityMapper);
-//
-//    cli.start();
+    //this is for a test commit
+  //  var scanner = new Scanner(System.in);
+  //  var fakeItems = new FakeItems();
+  //  var printStream = System.out;
+  //  IMapper cityMapper = new CityMapper();
+  //  var cli = new CLI(scanner, printStream, fakeItems, cityMapper);
+
+  //  cli.start();
   }
 }
